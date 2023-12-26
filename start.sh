bab18da7bf6f1fbb542046cdc28f7202e81676ed
Yevhenii Hurin <evgeny.gurin@gmail.com>
2023-12-27 01:14:26 +0200

    Verbose start.sh

--- start.sh
+++ start.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-# If /root/.ollama/ exists and it's not a symlink
+echo "Checking if /root/.ollama/ exists and it's not a symlink..."
+
 if [ -d "/root/.ollama/" ] && [ ! -L "/root/.ollama/" ]; then
+    echo "/root/.ollama/ exists and it's not a symlink. Removing it and creating a symlink..."
     rm -rf /root/.ollama/
     ln -s /runpod-volume/ /root/.ollama/
-# If /root/.ollama/ does not exist
+    echo "Symlink created."
 elif [ ! -e "/root/.ollama/" ]; then
+    echo "/root/.ollama/ does not exist. Creating a symlink..."
     ln -s /runpod-volume/ /root/.ollama/
+    echo "Symlink created."
 fi
 
+echo "Listing directories in /:"
+ls /
+
+echo "Killing any running ollama processes..."
 pgrep ollama | xargs kill
 
+echo "Starting ollama server..."
 ollama serve 2>&1 | tee ollama.server.log &
 # Store the process ID (PID) of the background command
 
 check_server_is_running() {
     # Replace "Listening" with the actual expected output
     if tail -n 1 ollama.server.log | grep -q "Listening"; then
+        echo "Server is running."
         return 0 # Success
     else
+        echo "Server is not running yet."
         return 1 # Failure
     fi
 }
 
 # Wait for the process to print "Listening"
+echo "Waiting for server to start..."
 while ! check_server_is_running; do
     sleep 1
 done
 
+echo "Pulling data with ollama..."
 ollama pull $1
+
+echo "Running runpod_wrapper.py..."
 python -u runpod_wrapper.py $1
